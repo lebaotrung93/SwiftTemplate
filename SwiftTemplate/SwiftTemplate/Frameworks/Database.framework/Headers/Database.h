d36b6dc5c60f87ac1d73e47bb7269a1b989776c3
Le Bao Trung (RBVH/ETM2) <Trung.LeBao@vn.bosch.com>
2020-04-24 10:23:44 +0700

    upgrade to swift 5
    - add Realm, Database
    - remove architecture before build & archire

---
+++ SwiftTemplate/SwiftTemplate/Frameworks/Database.framework/Headers/Database.h
@@ -0,0 +1,19 @@
+//
+//  Database.h
+//  Database
+//
+//  Created by Nguyen Thanh Hai on 6/18/17.
+//  Copyright © 2017 Nguyen Thanh Hai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Database.
+FOUNDATION_EXPORT double DatabaseVersionNumber;
+
+//! Project version string for Database.
+FOUNDATION_EXPORT const unsigned char DatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Database/PublicHeader.h>
+
+
